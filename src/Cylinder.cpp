41669e6d1da9dde10aca3a23875408638d549210
hedgehog719 <2024OOP001@tsri-201-197.lab.nycu.edu.tw>
2025-04-21 20:17:04 +0800

    add Lab7

--- src/Cylinder.cpp
+++ src/Cylinder.cpp
@@ -2,32 +2,34 @@
 # define CYLINDER_CPP
 
 # include "Cylinder.h"
-
+#include<iostream>
+#include<iomanip>
+#define PI 3.14159265358979323846
 double Cylinder :: SurfaceArea(){
     
-    return ;
+    return 2.0*height*PI*radius+PI*radius*radius*2;
 }
 
 double Cylinder :: Volume(){
-
-    return ;
+    return 1.0*PI*radius*radius*height;
 }
 
 double Cylinder :: Circumference(){
-
-    return ;
+    return 1.0*PI*radius*2;
 }
 
 istream & operator>>(istream & in, Cylinder & cldr)
 {
-
-    return ;
+    in>>cldr.radius>>cldr.height;
+    return in;
 }
 
 ostream & operator<<(ostream & out, Cylinder & cldr)
 {
-    
-    return ;
+    out<<"Circumference: "<<fixed<<setprecision(3)<<cldr.Circumference()<<endl;
+    out<<"SurfaceArea: "<<fixed<<setprecision(3)<<cldr.SurfaceArea()<<endl;
+    out<<"Volume: "<<fixed<<setprecision(3)<<cldr.Volume()<<endl;
+    return out;
 }
 
 # endif
